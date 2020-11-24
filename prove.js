634222f0dc31e76e0788d4a499fa30cf649c89ad
Mattia Tagliamonte <43723252+mattitagliamonte@users.noreply.github.com>
2020-11-24 12:25:17 +0100

    Add files via upload

---
+++ prove.js
@@ -0,0 +1,26 @@
+'use strict' //finchè siamo principianti è da utilizzare 
+
+
+alert("Ciaooo"); 
+//con la funzione alert possiamo illustrare un messaggio all'utente che apre la pagina;
+
+alert ("ciao a tutti");
+alert ("siii");
+
+alert("ciao a tutti ")
+alert ("siii")
+
+// le due coppie di alert sopra vengono viste d JS allo stesso modo ma non è sempre sensato omettere i ;
+//convenzionalmente tutte le istruzioni in JS sono seguite da ;
+let name = "matt";
+let message  = name;
+alert (message) ;
+//variabili,assumono i valori che assegnamo e vanno nominate in maniera intelligente
+
+const BIRTHDAY = "8 gennaio 2004";
+
+//le costanti conterranno sempre lo stesso valore
+let newname = "Giulio";
+
+alert( `operazione semplice = ${4 + 7}` ); // usando i backsticks e ${} è possibile compiere azioni di questo tipo 
+
