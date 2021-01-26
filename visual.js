0c46709fd241ceb9f629fdffb1e747d8e4d47386
Mattia Tagliamonte <43723252+mattitagliamonte@users.noreply.github.com>
2021-01-26 17:55:38 +0100

    Add files via upload

---
+++ visual.js
@@ -0,0 +1,5 @@
+alert("Buondì, facciamo un gioco");
+
+var tuoNome = window.promp("Inanzitutto dicci come ti chiami:") ;
+
+document.getElementById('titolo').innerHTML = "Eccoti finalmente "+ tuoNome;
